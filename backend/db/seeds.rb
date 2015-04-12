48471877a49ae193c2c25f792edfb47c98e3ee82
Jimmy Beaudoin <contact@jim-beaudoin.com>
2015-04-11 23:17:51 -0400

    Add: Backend Database Seed Data

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Student.create(name: "Melissa")
+Student.create(name: "Sophie")
+Student.create(name: "Marie-Claude")
