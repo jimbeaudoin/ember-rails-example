af9a41dac95eba5c5dcadee5a7c213a9c9a10192
Jimmy Beaudoin <contact@jim-beaudoin.com>
2015-04-11 21:49:54 -0400

    Add: Navigating Back to Root Integration Test (fail)

--- frontend/tests/integration/landing-page-test.js
+++ frontend/tests/integration/landing-page-test.js
@@ -18,3 +18,11 @@ test('Should welcome me to Boston Ember', function(assert) {
     assert.equal(find('h2#title').text(), 'Welcome to Ember Frontend');
   });
 });
+
+test('Should allow navigating back to root from another page', function(assert) {
+  visit('/about').then(function() {
+    click('a:contains("Home")').then(function() {
+      assert.notEqual(find('h3').text(), 'About');
+    });
+  });
+});
