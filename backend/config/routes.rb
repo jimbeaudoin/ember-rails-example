cf004811b05bffa3f22f488edb80bbf0507d100f
Jimmy Beaudoin <contact@jim-beaudoin.com>
2015-04-11 23:19:16 -0400

    Add: Backend Students Routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  namespace :api do
+    resources :students
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
